20924775078c761c51481ccf6ab88c915c1bc521
Leafsan <35473109+Leafsan@users.noreply.github.com>
2022-06-23 19:05:00 +0900

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ifstream inputFile( "../asstageData.txt", ifstream::binary);
+    inputFile.seekg(0, ifstream::end);
+    int fileSize = static_cast<int>(inputFile.tellg());
+    inputFile.seekg(0, ifstream::beg);
+    char* fileImage;
+    try
+    {
+        fileImage = new char[fileSize];
+    }
+    catch(const std::bad_alloc& e)
+    {
+        cout << "There is no file." << endl;
+        return 1;
+    }
+    inputFile.read(fileImage, fileSize);
+    cout.write(fileImage, fileSize);
+
+    return 0;
+}
